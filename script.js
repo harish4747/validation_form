2d72887ce8535c2afb2466d61430320f459050b5
harish4747 <harich4747@gmail.com>
2024-12-24 21:13:10 +0530

    feat :adding  new features

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
+// -------------------------------------- for Sigmup {
 var sno = 1001;
 let fname;
 let lname;
 let mail;
 let password;
-const details = [];
-
-function validateMail(input) {
-    let specialcharacter = false;
-    let requiredlength = false;
-    if ((input.includes(".com", input.length - 4)) && input.length > 4) {
-        requiredlength = true;
-    }
-    if (input.includes("@", 1)) {
-        specialcharacter = true;
-    }
-
-    if (requiredlength && specialcharacter) {
-        return input;
-    }
-    else {
-        window.alert("Please Enter Valid email");
-    }
-}
+let details = []
 
 function validateName(input, name) {
     if ((input == "")) {
@@ -32,6 +15,28 @@ function validateName(input, name) {
     }
     else {
         window.alert("invalid " + `${name}`);
+    }
+}
+function validateMail(input) {
+    if (input == "") {
+        window.alert("Please Enter Email");
+    }
+    else {
+        let specialcharacter = false;
+        let requiredlength = false;
+        if ((input.includes(".com", input.length - 4)) && input.length > 4) {
+            requiredlength = true;
+        }
+        if (input.includes("@", 1)) {
+            specialcharacter = true;
+        }
+
+        if (requiredlength && specialcharacter) {
+            return input;
+        }
+        else {
+            window.alert("Please Enter Valid email");
+        }
     }
 }
 
@@ -95,4 +100,68 @@ document.getElementById("submit").onclick = () => {
         finalOutput(fname, lname, mail, password);
     }
 
+}
+
+// -------------------------------------- }
+// -------------------------------------- for login { 
+
+let loginmail;
+let loginpassword;
+let firstoutput;
+let finaldisplay;
+
+function checkUser(loginmail, loginpassword) {
+    firstoutput = details.filter((val) => val.email == loginmail);
+    firstoutput.length != 0 ? checkPassword(loginmail, loginpassword) : window.alert("invalid user");
+
+}
+
+function checkPassword(finalOutput, loginpassword) {
+    finaldisplay = details.filter((val) => val.email == loginmail && val.password === loginpassword);
+    if (finaldisplay.length == 0) {
+        window.alert("wrong password");
+    }
+    else {
+        alert("welcome "+finaldisplay[0].firstName);
+        console.table(finaldisplay);
+        // console.table(typeof(finaldisplay));
+    }
+
+}
+
+
+
+document.getElementById("loginbutton").onclick = () => {
+    event.preventDefault();
+    loginmail = validateMail(document.getElementById("loginmail").value.trim().toLowerCase());
+    if (loginmail != undefined) {
+        loginpassword = validatePassword(document.getElementById("loginpassword").value.trim());
+    }
+    if (loginmail != undefined && loginpassword != undefined) {
+        checkUser(loginmail, loginpassword);
+    }
+
+}
+// -------------------------------------- }
+
+function choice(event,cho)
+{
+    let select1=document.getElementById("fform");
+    let select2=document.getElementById("sform");
+    let color1 =document.getElementById("login");
+    let color2 =document.getElementById("signup");
+    if(cho=="login") 
+    {
+        color1.style.backgroundColor="aqua";
+        color2.style.backgroundColor="grey";
+        select1.style.zIndex=1;
+        select2.style.zIndex=-1;
+    }
+    
+    else{
+        color1.style.backgroundColor="grey";
+        color2.style.backgroundColor="rgb(235, 73, 73)";
+        select2.style.zIndex=1;
+        select1.style.zIndex=-1;
+    }
 }
